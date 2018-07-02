e14fc19786470bc407491a233955487036fdc8b1
lilin@carzone.top <lilin@carzone.top>
2018-07-02 14:03:39 +0800

    Initial import

---
+++ src/main/java/com/example/koun/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.koun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
